16e91d95442521190c0a5931ec16ef3da5af087b
RaiqaJaved <111554677+RaiqaJaved@users.noreply.github.com>
2022-08-18 22:02:27 -0700

    Create codeone.cpp

---
+++ codeone.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+using namespace std;
+int main()
+
+{
+	int num;
+	
+	cout<<"please enter num"<<endl;
+	cin>>num;
+	
+	if(num%2==0)
+	{
+		cout<<"num is even";
+}
+    else
+	{
+		cout<<"num is odd";
+	}	
+}
