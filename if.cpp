d91cc1200f84900281fb1f7af8d35552f0acf0d3
RaiqaJaved <111554677+RaiqaJaved@users.noreply.github.com>
2022-08-19 06:50:48 -0700

    Create if.cpp

---
+++ if.cpp
@@ -0,0 +1,24 @@
+# include<iostream>
+using namespace std;
+int main()
+
+{
+ int value1;
+ cout<<"plese enter value1"<<endl;
+ cin>>value1;
+ 
+ int value2;
+ cout<<"please enter value2"<<endl;
+ cin>>value2;
+ 
+ if(value1==value2)
+ {
+ 	cout<<"right";
+ }
+ 
+ else
+ { 
+    cout<<"wrong"; 
+ }
+ 	
+}
