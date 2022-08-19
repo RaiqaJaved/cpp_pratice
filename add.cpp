d66b8a4aeabd6321f5170c5d457001b8e9f4da27
RaiqaJaved <111554677+RaiqaJaved@users.noreply.github.com>
2022-08-19 06:40:28 -0700

    Create add.cpp

---
+++ add.cpp
@@ -0,0 +1,19 @@
+# include<iostream>
+using namespace std;
+int main()
+
+{
+	int value1;
+	int value2;
+	
+	cout<<"please enter value1"<<endl;
+	cin>>value1;
+	
+	cout<<"please enter value2"<<endl;
+	cin>>value2;
+	
+	int result=value1+value2;
+
+
+    cout<<result;
+}
