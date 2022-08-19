1d9bd0508ab672d63d2e53fd3598ff0bb8836e4c
RaiqaJaved <111554677+RaiqaJaved@users.noreply.github.com>
2022-08-19 06:53:58 -0700

    Create square.cpp

---
+++ square.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+ {
+ 	int myvalue;
+ 	cout<<"enter some value to calculate square Root"<<endl;
+ 	cin>>myvalue;
+ 	cout<<sqrt(myvalue);
+ }
