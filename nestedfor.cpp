e0dba0501afb9476e10542d6d0705a61369132c7
RaiqaJaved <111554677+RaiqaJaved@users.noreply.github.com>
2022-08-19 06:57:37 -0700

    Create nestedfor.cpp

---
+++ nestedfor.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+using namespace std;
+int main ()
+  {
+	for(int i=1; i<10; i++)
+	   {
+	   	cout<<"www"<<endl;
+	   }
+	for(int j=1; j<10; j++)
+    	{
+    	 cout<<"pk"<<endl;	
+	    }
+	for(int k=1; k<10; k++)
+	   {
+	   	cout<<"123"<<endl;	
+	}
+}
